36fd5a061bbdc28bd3ff8e5f130d8467911a7954
MAL1S <karbushev_ivan@mail.ru>
2021-09-09 23:21:33 +0800

    Created viewPager2 Created expenses list item Created main activity design

--- app/src/main/java/com/example/expensesmanager/MainActivity.kt
+++ app/src/main/java/com/example/expensesmanager/MainActivity.kt
@@ -1,19 +1,64 @@
 package com.example.expensesmanager
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import android.widget.TableLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.expensesmanager.databinding.ActivityMainBinding
+import com.example.expensesmanager.ui.FragmentAdapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var pager: ViewPager2
+    private lateinit var adapter: FragmentAdapter
+    private lateinit var fm: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
-//        val actionBar = supportActionBar
-//        actionBar?.title = "July"
-        setSupportActionBar(findViewById(R.id.my_toolbar))
+        initViewPager()
+        init()
+    }
+
+    private fun init() {
+
+    }
+
+    private fun initViewPager() {
+        tabLayout = mBinding.tabLayout
+        pager = mBinding.viewPager
+
+        fm = supportFragmentManager
+        adapter = FragmentAdapter(fm, lifecycle)
+        pager.adapter = adapter
+
+        tabLayout.addTab(tabLayout.newTab().setText("Expenses"))
+        tabLayout.addTab(tabLayout.newTab().setText("Income"))
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                pager.currentItem = tab?.position!!
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+
+        pager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/expensesmanager/ui/FragmentAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.expensesmanager.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.expensesmanager.ui.expenses_fragment.ExpensesFragment
+import com.example.expensesmanager.ui.income_fragment.IncomeFragment
+
+class FragmentAdapter(fm: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fm, lifecycle) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            1 -> return ExpensesFragment()
+            2 -> return IncomeFragment()
+        }
+        return ExpensesFragment()
+    }
+}
